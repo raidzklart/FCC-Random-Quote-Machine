72163ed27a1fd990fbf06486bbff4f86b79dac72
Thomas Christopher <thomas.j.christopher@gmail.com>
2019-07-09 15:38:23 +0100

    Updates

---
+++ precache-manifest.5bffea65e1c5b11e810434ecb55c4665.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b76f635b53c576dcc376a523a874c77",
+    "url": "/FCC-Random-Quote-Machine/index.html"
+  },
+  {
+    "revision": "554b64dfd4551330fdae",
+    "url": "/FCC-Random-Quote-Machine/static/css/main.32a9652f.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/FCC-Random-Quote-Machine/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "554b64dfd4551330fdae",
+    "url": "/FCC-Random-Quote-Machine/static/js/main.b9b8d80c.chunk.js"
+  },
+  {
+    "revision": "da5be31b3417d3493684",
+    "url": "/FCC-Random-Quote-Machine/static/js/runtime~main.2fe9d678.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/FCC-Random-Quote-Machine./precache-manifest.4e91fe9d16e95d18538fa1fc002d2387.js"
+  "/FCC-Random-Quote-Machine/precache-manifest.5bffea65e1c5b11e810434ecb55c4665.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/FCC-Random-Quote-Machine./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/FCC-Random-Quote-Machine/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
